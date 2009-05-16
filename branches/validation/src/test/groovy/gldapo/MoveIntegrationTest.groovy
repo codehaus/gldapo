6e494dc2e2e9ff47ec9719f4c45c55cc3302a683
ldaley <ldaley@d6b9759c-a534-0410-b079-b5b818326a0d>
2009-05-16 02:12:33 +0000

    • Creating a branch for the validation features to be added

---
+++ branches/validation/src/test/groovy/gldapo/MoveIntegrationTest.groovy
@@ -0,0 +1,231 @@
+package gldapo
+import gldapo.schema.annotation.GldapoSynonymFor
+import gldapo.schema.annotation.GldapoNamingAttribute
+
+public class MoveIntegrationTest extends AbstractGldapoIntegrationTest
+{
+    def schemaClasses = [MoveIntegrationTestPerson]
+
+    void testMoveUnmodified() {
+        def p = new MoveIntegrationTestPerson()
+        def cn = "move"
+        def sn = "sn"
+        def brdn = "cn=$cn"
+
+        def constantLdif = """
+            sn: $sn
+            objectClass: person
+            objectClass: top
+        """
+
+        p.brdn = brdn
+        p.objectClass = ["top", "person"]
+        p.cn = cn
+        p.sn = sn
+        p.create()
+
+        assertEqualsLdif(brdn, """
+            cn: $cn
+            $constantLdif
+        """)
+
+        def oldRdn = brdn
+        cn = "moved"
+        brdn = "cn=$cn"
+        p.move(brdn)
+
+        assertNull(getEntry(oldRdn))
+
+        assertEqualsLdif(brdn, """
+            $constantLdif
+            cn: moved
+        """)
+
+    }
+
+    void testMoveModified() {
+        def p = new MoveIntegrationTestPerson()
+        def cn = "move"
+        def sn = "sn"
+        def brdn = "cn=$cn"
+
+        def constantLdif = """
+            objectClass: person
+            objectClass: top
+        """
+
+        p.brdn = brdn
+        p.objectClass = ["top", "person"]
+        p.cn = cn
+        p.sn = sn
+        p.create()
+
+        assertEqualsLdif(brdn, """
+            $constantLdif
+            cn: $cn
+            sn: $sn
+        """)
+
+        def oldRdn = brdn
+        cn = "moved"
+        sn = "sn_changed"
+        brdn = "cn=$cn"
+
+        p.sn = sn
+        p.move(brdn)
+
+        assertNull(getEntry(oldRdn))
+
+        assertEqualsLdif(brdn, """
+            $constantLdif
+            cn: $cn
+            sn: $sn
+        """)
+    }
+
+    void testMoveUsingNamingAttribute() {
+        
+        importEntry(ou: "moveByNaming", """
+            objectclass: top
+            objectclass: organizationalUnit
+        """)
+        
+        def cn = "moveByNaming"
+        def sn = "sn"
+        def parent = "ou=moveByNaming"
+        def brdn = "cn=$cn,${parent}"
+        def objectClass = ["top", "person"]
+        
+        def constantLdif = """
+            objectClass: person
+            objectClass: top
+        """
+        
+        def p = new MoveIntegrationTestPerson(
+            cn: cn,
+            sn: sn,
+            objectClass: objectClass,
+            parent: parent
+        )
+
+        p.create()
+
+        assertEqualsLdif(brdn, """
+            $constantLdif
+            cn: $cn
+            sn: $sn
+        """)
+
+        def oldRdn = brdn
+        cn = "movedByNaming"
+        brdn = "cn=$cn,${parent}"
+
+        p.move(cn, null)
+
+        assertNull(getEntry(oldRdn))
+
+        assertEqualsLdif(brdn, """
+            $constantLdif
+            cn: $cn
+            sn: $sn
+        """)
+        
+        oldRdn = brdn
+        brdn = "cn=$cn"
+
+        p.move(null, "")
+
+        assertNull(getEntry(oldRdn))
+
+        assertEqualsLdif(brdn, """
+            $constantLdif
+            cn: $cn
+            sn: $sn
+        """)
+        
+    }
+    
+    void testMoveUsingNamingAttributeAndParent() {
+        def cn = "moveByNamingAndParent"
+        def sn = "sn"
+        def brdn = "cn=$cn"
+        def objectClass = ["top", "person"]
+        def constantLdif = """
+            objectClass: person
+            objectClass: top
+        """
+
+        def p = new MoveIntegrationTestPerson(
+            cn: cn,
+            sn: sn,
+            objectClass: objectClass
+        )
+        
+        p.create()
+
+        importEntry(ou: "moveByNamingAndParentSub", """
+            objectclass: top
+            objectclass: organizationalUnit
+        """)
+        
+        cn = "movedByNamingAndParent"
+        sn = "changed"
+        p.sn = "changed"
+        
+        p.move(cn, "ou=moveByNamingAndParentSub")
+        
+        assertEqualsLdif("cn=$cn,ou=moveByNamingAndParentSub", """
+            $constantLdif
+            cn: $cn
+            sn: $sn
+        """)
+        
+    }
+    
+    void testMoveUsingParent() {
+        def cn = "moveByParent"
+        def sn = "sn"
+        def brdn = "cn=$cn"
+        def objectClass = ["top", "person"]
+        def constantLdif = """
+            objectClass: person
+            objectClass: top
+        """
+
+        def p = new MoveIntegrationTestPerson(
+            cn: cn,
+            sn: sn,
+            objectClass: objectClass
+        )
+        
+        p.create()
+
+        importEntry(ou: "moveByParentSub", """
+            objectclass: top
+            objectclass: organizationalUnit
+        """)
+        
+        sn = "changed"
+        p.sn = "changed"
+        
+        p.move(null, "ou=moveByParentSub")
+        
+        assertEqualsLdif("cn=$cn,ou=moveByParentSub", """
+            $constantLdif
+            cn: $cn
+            sn: $sn
+        """)
+        
+        p = new MoveIntegrationTestPerson()
+        shouldFail(gldapo.exception.GldapoException) {
+            p.move(null, "ou=moveByParentSub")
+        }
+        
+    }
+}
+
+class MoveIntegrationTestPerson {
+    Set<String> objectClass
+    String sn
+    @GldapoNamingAttribute String cn
+}
